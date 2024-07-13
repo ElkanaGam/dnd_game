caa78b52530aca975b9c3b97a080d17aa28c20f1
ElkanaGam <elkana1234@gmail.com>
2024-07-13 22:35:15 +0300

    add files

---
+++ script.js
@@ -0,0 +1,81 @@
+$(function() {
+    var canvas = document.getElementById("gameBoard");
+    var ctx = canvas.getContext("2d");
+
+    var boardSize = 10;
+    var cellSize = canvas.width / boardSize;
+    
+    function drawBoard() {
+        for (var i = 0; i < boardSize; i++) {
+            for (var j = 0; j < boardSize; j++) {
+                ctx.strokeRect(i * cellSize, j * cellSize, cellSize, cellSize);
+            }
+        }
+    }
+
+    drawBoard();
+
+    var player = {
+        x: 0,
+        y: 0,
+        radius: 0.25 * cellSize, // Diameter will be 0.5 * cellSize
+        color: "blue"
+    };
+
+    function drawPlayer() {
+        ctx.beginPath();
+        ctx.arc(
+            player.x * cellSize + cellSize / 2, // Center x within the cell
+            player.y * cellSize + cellSize / 2, // Center y within the cell
+            player.radius,
+            0,
+            2 * Math.PI
+        );
+        ctx.fillStyle = player.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+    
+
+    function clearPlayer() {
+        ctx.clearRect(
+            player.x * cellSize, 
+            player.y * cellSize, 
+            cellSize, 
+            cellSize
+        );
+        ctx.strokeRect(player.x*cellSize, player.y*cellSize, cellSize, cellSize);
+    }
+
+    drawPlayer();
+
+    function movePlayerTo(cellX, cellY) {
+        clearPlayer();
+        player.x = cellX;
+        player.y = cellY;
+        drawPlayer();
+    }
+
+    // Add drag-and-drop functionality using jQuery
+    $("#gameBoard").on("mousedown", function(e) {
+        var rect = canvas.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+        var cellX = Math.floor(x / cellSize);
+        var cellY = Math.floor(y / cellSize);
+
+        if (cellX === player.x && cellY === player.y) {
+            $(document).on("mousemove", function(e) {
+                var x = e.clientX - rect.left;
+                var y = e.clientY - rect.top;
+                var cellX = Math.floor(x / cellSize);
+                var cellY = Math.floor(y / cellSize);
+                movePlayerTo(cellX, cellY);
+            });
+
+            $(document).on("mouseup", function() {
+                $(document).off("mousemove");
+            });
+        }
+    });
+});
